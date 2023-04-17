8174d807ada14b9cb1be1128b8ef5538d1a278d5
WelcomeToApollo <130937930+WelcomeToApollo@users.noreply.github.com>
2023-04-17 23:08:39 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -70,6 +70,30 @@ document.addEventListener("DOMContentLoaded", function () {
     updateEndorsementRequired();
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const updateEndorsementRequired = (yesId, noId, expiryId, levelId = null) => {
+        const yes = document.getElementById(yesId);
+        const no = document.getElementById(noId);
+        const expiry = document.getElementById(expiryId);
+        const level = levelId ? document.getElementById(levelId) : null;
 
+        function setRequired() {
+            expiry.required = yes.checked;
+            if (level) level.disabled = !yes.checked;
+        }
 
+        yes.addEventListener("change", setRequired);
+        no.addEventListener("change", setRequired);
+
+        setRequired();
+    };
+
+    updateEndorsementRequired("yes_opito_bosiet", "no_opito_bosiet", "opito_bosiet_expiry");
+    updateEndorsementRequired("yes_gwo_sea_survival", "no_gwo_sea_survival", "gwo_sea_survival_expiry");
+    updateEndorsementRequired("yes_gwo_working_heights", "no_gwo_working_heights", "gwo_working_heights_expiry");
+    updateEndorsementRequired("yes_gwo_manual_handling", "no_gwo_manual_handling", "gwo_manual_handling_expiry");
+    updateEndorsementRequired("yes_stcw_fast_rescue_crafts", "no_stcw_fast_rescue_crafts", "stcw_fast_rescue_crafts_expiry");
+    updateEndorsementRequired("yes_certificate_competence", "no_certificate_competence", "certificate_competence_expiry");
+    updateEndorsementRequired("yes_helideck_certification", "no_helideck_certification", "helideck_certification_expiry", "helideck_level");
+});
 
