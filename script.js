8fbc31c9cc533152100d4d3c9fef319fa294c105
WelcomeToApollo <130937930+WelcomeToApollo@users.noreply.github.com>
2023-04-20 23:24:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -31,78 +31,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const yesRadio = document.getElementById("yes");
-    const noRadio = document.getElementById("no");
-    const seamansBookNumber = document.getElementById("seamans_book_number");
-    const seamansBookExpiry = document.getElementById("seamans_book_expiry");
-
-    function updateRequired() {
-        if (yesRadio.checked) {
-            seamansBookNumber.required = true;
-            seamansBookExpiry.required = true;
-        } else {
-            seamansBookNumber.required = false;
-            seamansBookExpiry.required = false;
-        }
+function updateRequiredFields(yesId, noId, field1Id, field2Id = null) {
+    const yesRadio = document.getElementById(yesId);
+    const noRadio = document.getElementById(noId);
+    const field1 = document.getElementById(field1Id);
+    const field2 = field2Id ? document.getElementById(field2Id) : null;
+  
+    function setRequired() {
+      field1.required = yesRadio.checked;
+      if (field2) field2.required = yesRadio.checked;
     }
-
-    yesRadio.addEventListener("change", updateRequired);
-    noRadio.addEventListener("change", updateRequired);
-
-    updateRequired();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const yesEndorsement = document.getElementById("yes_endorsement");
-    const noEndorsement = document.getElementById("no_endorsement");
-    const luxEndorsementNumber = document.getElementById("lux_endorsement_number");
-    const luxEndorsementExpiry = document.getElementById("lux_endorsement_expiry");
-
-    function updateEndorsementRequired() {
-        if (yesEndorsement.checked) {
-            luxEndorsementNumber.required = true;
-            luxEndorsementExpiry.required = true;
-        } else {
-            luxEndorsementNumber.required = false;
-            luxEndorsementExpiry.required = false;
-        }
-    }
-
-    yesEndorsement.addEventListener("change", updateEndorsementRequired);
-    noEndorsement.addEventListener("change", updateEndorsementRequired);
-
-    updateEndorsementRequired();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const updateEndorsementRequired = (yesId, noId, expiryId, levelId = null) => {
-        const yes = document.getElementById(yesId);
-        const no = document.getElementById(noId);
-        const expiry = document.getElementById(expiryId);
-        const level = levelId ? document.getElementById(levelId) : null;
-
-        function setRequired() {
-            expiry.required = yes.checked;
-            if (level) level.disabled = !yes.checked;
-        }
-
-        yes.addEventListener("change", setRequired);
-        no.addEventListener("change", setRequired);
-
-        setRequired();
-    };
-
-    updateEndorsementRequired("yes_opito_bosiet", "no_opito_bosiet", "opito_bosiet_expiry");
-    updateEndorsementRequired("yes_gwo_sea_survival", "no_gwo_sea_survival", "gwo_sea_survival_expiry");
-    updateEndorsementRequired("yes_gwo_working_heights", "no_gwo_working_heights", "gwo_working_heights_expiry");
-    updateEndorsementRequired("yes_gwo_manual_handling", "no_gwo_manual_handling", "gwo_manual_handling_expiry");
-    updateEndorsementRequired("yes_stcw_fast_rescue_crafts", "no_stcw_fast_rescue_crafts", "stcw_fast_rescue_crafts_expiry");
-    updateEndorsementRequired("yes_certificate_competence", "no_certificate_competence", "certificate_competence_expiry");
-    updateEndorsementRequired("yes_helideck_certification", "no_helideck_certification", "helideck_certification_expiry", "helideck_level");
-});
-
+  
+    yesRadio.addEventListener("change", setRequired);
+    noRadio.addEventListener("change", setRequired);
+    setRequired();
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    updateRequiredFields("yes_lux_seamans_book", "no_lux_seamans_book", "seamans_book_number", "seamans_book_expiry");
+    updateRequiredFields("yes_endorsement", "no_endorsement", "lux_endorsement_number", "lux_endorsement_expiry");
+    updateRequiredFields("yes_opito_bosiet", "no_opito_bosiet", "opito_bosiet_expiry");
+    updateRequiredFields("yes_gwo_sea_survival", "no_gwo_sea_survival", "gwo_sea_survival_expiry");
+    updateRequiredFields("yes_gwo_working_heights", "no_gwo_working_heights", "gwo_working_heights_expiry");
+    updateRequiredFields("yes_gwo_manual_handling", "no_gwo_manual_handling", "gwo_manual_handling_expiry");
+    updateRequiredFields("yes_stcw_fast_rescue_crafts", "no_stcw_fast_rescue_crafts", "stcw_fast_rescue_crafts_expiry");
+    updateRequiredFields("yes_certificate_competence", "no_certificate_competence", "certificate_competence_expiry");
+    updateRequiredFields("yes_helideck_certification", "no_helideck_certification", "helideck_certification_expiry", "helideck_level");
+  });
+  
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("myForm");
     const errorMessage = document.getElementById("errorMessage");
@@ -117,9 +73,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  fetch('countries_dropdown.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('countries-dropdown').innerHTML = data;
-      });
+
+      
+
+      
   
